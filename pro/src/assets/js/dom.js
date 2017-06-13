8b199fd66f44b6e93b756043b054106a10bdeec2
Geek.Yu <1098654043@qq.com>
2017-06-13 20:24:04 +0800

    首页公告横向无缝滚动

--- pro/src/assets/js/dom.js
+++ pro/src/assets/js/dom.js
@@ -15,7 +15,96 @@ export function addClass(el, className) {
   el.className = newClass.join(' ')
 }
 
-//ªÒ»°‘™Àÿœ‡∂‘”⁄‰Ø¿¿∆˜µƒ∫·◊¯±Í
+//ËÆæÁΩÆcssÊ†∑Âºè
+export function css(obj,attr,value){
+  if(arguments.length==2){
+    if(attr=='scale'|| attr=='rotate'|| attr=='rotateX'||attr=='rotateY'||attr=='scaleX'||attr=='scaleY'||attr=='translateY'||attr=='translateX'){
+      if(!obj.$Transform)obj.$Transform={};
+      switch(attr){
+        case 'scale':
+        case 'scaleX':
+        case 'scaleY':
+          return typeof(obj.$Transform[attr])=='number'?obj.$Transform[attr]:100;
+          break;
+        default:
+          return obj.$Transform[attr]?obj.$Transform[attr]:0;
+      }
+    }
+    var current=getStyle(obj,attr);
+    return attr=='opacity'?Math.round(parseFloat(current)*100):parseInt(current);   
+  }else if(arguments.length==3){
+    switch(attr){
+      case 'scale':
+      case 'scaleX':
+      case 'scaleY':
+      case 'rotate':
+      case 'rotateX':
+      case 'rotateY':
+      case 'translateZ':
+      case 'translateX':
+      case 'translateY':
+        setCss3(obj,attr,value);
+        break;
+      case 'width':
+      case 'height':
+      case 'paddingLeft':
+      case 'paddingTop':
+      case 'paddingRight':
+      case 'paddingBottom':
+        value=Math.max(value,0);
+      case 'left':
+      case 'top':
+      case 'marginLeft':
+      case 'marginTop':
+      case 'marginRight':
+      case 'marginBottom':
+        obj.style[attr]=typeof(value=='string')?value:value+'px';
+        break;
+      case 'opacity':
+        obj.style.filter="alpha(opacity:"+value+")";
+        obj.style.opacity=value/100;
+        break;
+      default:
+        obj.style[attr]=value;
+    }
+  }
+  return function(attr_in,value_in){css(obj,attr_in,value_in)};
+};
+
+//ÂÖºÂÆπcss3Ê†∑Âºè
+export function setCss3(obj, attr, value){
+  var str='';
+  var val='';
+  var arr=['Webkit','Moz','O','ms',''];
+  if(!obj['$Transform']){
+    obj['$Transform']={};
+  }
+  obj['$Transform'][attr]=parseInt(value);
+  for(str in obj['$Transform']){
+    switch(str){
+      case 'scale':
+      case 'scaleX':
+      case 'scaleY':
+        val+=str+'('+(obj['$Transform'][str]/100)+')';  
+        break;
+      case 'rotate':
+      case 'rotateX':
+      case 'rotateY':
+        val+=str+'('+(obj['$Transform'][str])+'deg)'; 
+        break;
+      case 'translateX':
+      case 'translateY':
+      case 'translateZ':
+        val+=str+'('+(obj['$Transform'][str])+'px)';  
+        break;
+    }
+  }
+  for(var i=0;i<arr.length;i++){
+    obj.style[arr[i]+'Transform']=val;
+  } 
+};  
+
+//Ëé∑ÂèñÂÖÉÁ¥†Áõ∏ÂØπ‰∫éÊµèËßàÂô®ÁöÑÊ®™ÂùêÊ†á
 export function getLeftToBrowser(e) {
   var offset=e.offsetLeft; 
   if(e.offsetParent!=null) offset+=getLeftToBrowser(e.offsetParent); 
@@ -23,24 +112,58 @@ export function getLeftToBrowser(e) {
 }
 
 /** 
-* @description  ¬º˛∞Û∂®£¨ºÊ»›∏˜‰Ø¿¿∆˜ 
-* @param target  ¬º˛¥•∑¢∂‘œÛ 
-* @param type  ¬º˛ 
-* @param func  ¬º˛¥¶¿Ì∫Ø ˝ 
+* @description È¶ñÈ°µÁöÑÂÖ¨Âëä 
+* @param obj Ë¶ÅÊªöÂä®ÁöÑÁà∂ÂÆπÂô® 
+* @param dis ÊØèÊ¨°ËøêÂä®ÁöÑË∑ùÁ¶ª 
+* @param msec ÂÆöÊó∂Âô®È¢ëÁéá 
+*/ 
+export function autoNoticeHorizontal(obj,onOFF,dis,msec){
+  var oLi=obj.children;
+  var iL=oLi.length*2;
+  var iW=0;
+  var iNow=0;
+  var onOFF = onOFF||false;
+  var indexInterval = null;
+  if(onOFF&&!myInterval){
+    console.log("ËøõÂÖ•ÂÆöÊó∂Âô®")
+      css(obj,'translateX',0);
+      clearInterval(myInterval);
+      myInterval = null;
+      return;
+  }    
+  obj.innerHTML+=obj.innerHTML;
+  for(var i=0;i<obj.children.length;i++){
+    iW=obj.children[i].offsetWidth*iL;
+  }   
+  obj.style.width=iW+'px';
+  
+  indexInterval = setInterval(function(){
+    if(css(obj,'translateX')<=-iW/2){
+      css(obj,'translateX',-14);  
+    }
+    css(obj,'translateX',css(obj,'translateX')-(dis||1));
+  },msec||25);
+};
+
+/** 
+* @description ‰∫ã‰ª∂ÁªëÂÆöÔºåÂÖºÂÆπÂêÑÊµèËßàÂô® 
+* @param target ‰∫ã‰ª∂Ëß¶ÂèëÂØπË±° 
+* @param type ‰∫ã‰ª∂ 
+* @param func ‰∫ã‰ª∂Â§ÑÁêÜÂáΩÊï∞ 
 */ 
 export function addEvent(target, type, func) { 
-    if (target.addEventListener) //∑«ie ∫Õie9 
+    if (target.addEventListener) //Èùûie Âíåie9 
         target.addEventListener(type, func, false); 
-    else if (target.attachEvent) //ie6µΩie8 
+    else if (target.attachEvent) //ie6Âà∞ie8 
         target.attachEvent("on" + type, func); 
     else target["on" + type] = func; //ie5 
 }; 
 
 /** 
-* @description  ¬º˛“∆≥˝£¨ºÊ»›∏˜‰Ø¿¿∆˜ 
-* @param target  ¬º˛¥•∑¢∂‘œÛ 
-* @param type  ¬º˛ 
-* @param func  ¬º˛¥¶¿Ì∫Ø ˝ 
+* @description ‰∫ã‰ª∂ÁßªÈô§ÔºåÂÖºÂÆπÂêÑÊµèËßàÂô® 
+* @param target ‰∫ã‰ª∂Ëß¶ÂèëÂØπË±° 
+* @param type ‰∫ã‰ª∂ 
+* @param func ‰∫ã‰ª∂Â§ÑÁêÜÂáΩÊï∞ 
 */ 
 export function removeEvent(target, type, func){ 
     if (target.removeEventListener) 
@@ -50,6 +173,7 @@ export function removeEvent(target, type, func){
     else target["on" + type] = null; 
 };  
 
+//Ëé∑ÂèñdataÔºåÊãºÊé•
 export function getData(el, name, val) {
   const prefix = 'data-'
   if (val) {
@@ -59,7 +183,6 @@ export function getData(el, name, val) {
 }
 
 let elementStyle = document.createElement('div').style
-
 let vendor = (() => {
   let transformNames = {
     webkit: 'webkitTransform',
--- pro/src/components/common/noticeBar.vue
+++ pro/src/components/common/noticeBar.vue
@@ -3,7 +3,7 @@
 	    <div class="m-notice-bar">
             <div class="m-tit">今日成交：</div><!-- 标题 -->
             <div class="m-notice-info">
-                <ul class="u-notice-lst f__clearfix">
+                <ul class="u-notice-lst f__clearfix" ref="noticeList">
                     <li class="u-notice-item" v-for="item in noticeList">{{item.title}}</li>
                 </ul>
             </div>
@@ -12,7 +12,8 @@
 </template>
 
 <script>
-
+    
+    import {autoNoticeHorizontal} from "assets/js/dom.js";
 	export default {
         name: "noticeBar",
         // 数据
@@ -25,7 +26,19 @@
             }
         },
         mounted(){
-
+            this.$nextTick(function(){
+                setTimeout(() => {
+                    let noticeList = this.$refs.noticeList;
+                    autoNoticeHorizontal(noticeList);
+                },2000)
+            })
+        },
+        activated(){
+            
+        },
+        deactivated(){
+            let noticeList = this.$refs.noticeList;
+            autoNoticeHorizontal(noticeList,true);
         },
         // 自定义函数(方法)
         methods: {
@@ -59,6 +72,7 @@
         .m-notice-info
             width @width - 138px
             margin-left 138px
+            overflow hidden
             .u-notice-lst
                 width 500%
                 height 48px
