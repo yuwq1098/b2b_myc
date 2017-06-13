87328c6114c629ea56f0ea9a95ae8ee9c18bcdfe
Geek.Yu <1098654043@qq.com>
2017-06-13 19:23:38 +0800

    首页品牌选择组件先完成

--- pro/src/components/brandSel/brandSelect.vue
+++ pro/src/components/brandSel/brandSelect.vue
@@ -4,7 +4,23 @@
 	    <div class="m-category f__clearfix">
             <div class="m-brand-wrap f__fl">
                 <div class="u-hd">
-                    <a href="javascript:;" class="u-lk">品牌<i class="iconfont icon-caretdown"></i></a>
+                    <a href="javascript:;" @mouseover="isShowAllBrand=true" class="u-lk" :class="{'on':isShowAllBrand}">品牌<i class="iconfont icon-caretdown"></i></a>
+                    <div class="m-all-brand-box" @mouseleave="isShowAllBrand=false" id="js__all_brand" v-show="isShowAllBrand">
+                        <div class="m-brand-group" v-for="n in gpLength" :class="{'last-group':n==gpLength}">
+                            <div class="u-gp-hd">
+                                <ul class="u-gup-hd-lst f__clearfix">
+                                    <li class="u-hd-item" v-for="(item,index) in brandList" v-if="index>=(7*(n-1))&&index<=(7*n)-1" :class="{'on':index==(7*(n-1))}">{{item.title}}</li>
+                                </ul>
+                            </div>
+                            <div class="u-gp-con" :class="{'last-item':n==gpLength}">
+                                <ul class="u-gp-lst f__clearfix" v-for="(group,index) in brandList" v-if="index>=(7*(n-1))&&index<=(7*n)-1" :class="{'active':index==(7*(n-1))}">
+                                    <li class="u-gp-item" v-for="item in group.items">
+                                        <a href="javascript:;" class="u-lk">{{item.name}}</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div><!-- 所有品牌展示的盒子 -->
                 </div>
                 <div class="u-con f__clearfix" v-if="hotBrand.length>0">
                     <a href="javascript:;" class="u-lk" v-for="item in hotBrand" :data-brand-id="item.id" >
@@ -12,6 +28,7 @@
                         <p class="u-tit">{{item.name}}</p>
                     </a>
                 </div>
+                
             </div>
             <div class="m-price-wrap f__fl">
                 <div class="u-hd">
@@ -34,7 +51,8 @@
 </template>
 
 <script>
-
+    
+    import $ from "jquery"
     import api from "api/getData.js"
     import {dataToJson} from "assets/js/util.js"
     import {brandInfo} from "base/class/brand.js"
@@ -50,12 +68,19 @@
                 carBrandMap: {},
                 hotBrand: [],
                 brandList: [],
+                isShowAllBrand: false,
             }
         },
         mounted(){
             //获取汽车品牌列表
             this._getB2BCarBrandList();
             // this._getCarSeries();
+        },
+        computed:{
+            // 仅读取，值只须为函数
+            gpLength: function () {
+                return (Math.ceil(this.brandList.length/7))
+            },
         },
         // 自定义函数(方法)
         methods: {
@@ -67,6 +92,9 @@
                     //热门品牌列表
                     this.hotBrand = this.carBrandMap.hotBrand;
                     this.brandList = this.carBrandMap.brandList;
+                    
+                    //显示更多的品牌信息的事件
+                    this._allBrandevent();
                 })
             },
             //使用b2c抽象类完成brandInfo
@@ -112,7 +140,6 @@
                 }
                 return brandMap;
             },
-
             //根据车品牌获取车系
             _getCarSeries(){
                 let [data] = [{}]
@@ -120,6 +147,29 @@
                 api.getCarSeriesByBrand(data).then((res) => {
                     console.log("根据车品牌获取车系",dataToJson(res.data));
                 })
+            },
+            //显示更多的品牌信息的事件
+            _allBrandevent(){
+                setTimeout(() => {
+                    let js__all_brand = $("#js__all_brand");
+                    let brandGroup = js__all_brand.find(".m-brand-group");
+                    let groupHeadList = brandGroup.find(".u-gup-hd-lst");
+                    let groupContent = brandGroup.find(".u-gp-con");
+                    
+                    groupHeadList.find(".u-hd-item").on("mouseover",function(){
+                        let me = $(this);
+                        let index = me.index();
+                        let parentDom = me.parent(".u-gup-hd-lst");
+                        let parentHead = me.parents(".u-gp-hd")
+                        let groupCon = parentHead.siblings(".u-gp-con");
+                        let groupConList = groupCon.find(".u-gp-lst");
+                        let currentConList = groupConList.eq(index);
+
+                        me.addClass("on").siblings(".u-hd-item").removeClass("on");
+                        currentConList.addClass("active").siblings().removeClass("active");
+                          
+                    });
+                },20)
             },
         },
         // 在当前模块注册组件
@@ -137,7 +187,6 @@
         height 284px
         _border(#f0f0f0,2px)
         _boxShadow(12px,rgba(0,0,0,.15))
-        overflow hidden
         margin-bottom 30px
         & .u-hd
             height 62px
@@ -148,7 +197,7 @@
             a,a:hover
                 color #313536
                 cursor default
-        .m-brand-wrap .u-hd i
+        .m-brand-wrap .u-hd .iconfont
             color #313536
             font-size 14px
             margin-left 3px
@@ -158,12 +207,80 @@
             _border(top,#ededed,1px)
         .m-brand-wrap
             width 850px
+            position relative
             .u-hd
-                .u-lk:hover
+                _transitionAll(.1s)
+                .iconfont
+                    _display(inline-block)
+                    _transformOrigin(,47.5%)
+                .u-lk:hover,
+                .u-lk.on
                     color $c_blue
                     cursor pointer
                     .iconfont
                         color @color
+                        _rotate(180deg)
+                        -webkit-transition all .1s linear,rotate .15s ease-in-out 
+                        -moz-transition all .1s linear,rotate .15s ease-in-out 
+                        transition all .1s linear,rotate .15s ease-in-out 
+                .m-all-brand-box
+                    position absolute
+                    top 62px
+                    left 0
+                    width 850px - 4px - 40px
+                    padding 20px
+                    min-height 200px
+                    border 2px solid $c_blue
+                    background #fff
+                    z-index 9
+                    &::before
+                        content ""
+                        _display()
+                        width 850px
+                        height 64px
+                        position absolute
+                        top -64px
+                        left -2px
+                        cursor pointer
+                    .m-brand-group
+                        margin-bottom 15px
+                        &.last-group
+                            margin-bottom 0
+                        .u-gp-hd
+                            .u-hd-item
+                                background #f2f2f2
+                                width 28px
+                                text-align center
+                                height 28px
+                                line-height 28px
+                                _display()
+                                font-size 16px
+                                color #545454
+                                margin-right 12px
+                                _borderRadius()
+                                float left
+                                cursor pointer
+                                &.on
+                                    background $c_blue
+                                    color #f4f4f4
+                        .u-gp-con
+                            margin-top 10px
+                            min-height 30px
+                            padding-bottom 15px
+                            _border(bottom,#e2e2e2)
+                            &.last-item
+                                padding-bottom 0
+                                border none
+                            .u-gp-lst
+                                _display(none)
+                                &.active
+                                    _display()
+                                .u-gp-item
+                                    float left
+                                    height 30px
+                                    line-height 30px
+                                    margin-right 15px
+
             .u-con
                 width @width - 50px
                 padding 18px 25px
