fbd9fa8f9c0811fa34a7bb7f6774f21473205b00
Geek.Yu <1098654043@qq.com>
2017-06-30 21:01:33 +0800

    代码更新

--- pro/src/page/member/car-source/manage/index.vue
+++ pro/src/page/member/car-source/manage/index.vue
@@ -7,7 +7,42 @@
             <div slot="content">
                 
                 <member-inner>
-                    <p>我的车源</p>
+                    <div class="m-source-con">
+                        <div class="m-tab-box f__clearfix">
+                            <a href="javascript:;" 
+                                @click="tabChange(1)"
+                                :class="{'on':tabShowIndex==1}"
+                                class="u-tab-lk">在售
+                            </a>
+                            <a href="javascript:;" 
+                                @click="tabChange(2)"
+                                :class="{'on':tabShowIndex==2}"
+                                class="u-tab-lk">已售
+                            </a>
+                            <a href="javascript:;" 
+                                @click="tabChange(3)"
+                                :class="{'on':tabShowIndex==3}"
+                                class="u-tab-lk">未上架
+                            </a>
+                        </div><!-- 切换 -->
+                        <div class="m-tab-wrap">
+                            <div class="m-tab-con"
+                                v-if="tabShowIndex==1"
+                                >
+                                在售
+                            </div><!-- 在售 -->
+                            <div class="m-tab-con"
+                                v-if="tabShowIndex==2"
+                                >
+                                已售
+                            </div><!-- 已售 -->
+                            <div class="m-tab-con"
+                                v-if="tabShowIndex==3"
+                                >
+                                未上架
+                            </div><!-- 未上架 -->
+                        </div>
+                    </div>
                 </member-inner>
 
             </div><!-- 车源管理内容 -->
@@ -33,12 +68,44 @@
         // 数据
         data() {
             return{
-                
+                // 选项卡显示
+                tabShowIndex: "",
             }
+        },
+        //生命周期,开始的时候
+        created(){
+            
+        },
+        mounted(){
+
+        },
+        activated(){
+            this.tabShowIndex = this.$router.currentRoute.query.tabIndex||1;
+        },
+        // 退出的生命周期钩子
+        deactivated(){
+            
+        },
+        // 属性值计算
+        computed:{
+
+        },
+        // 数据侦听
+        watch:{
+
         },
         // 自定义函数(方法)
         methods: {
-            
+            // 选项卡切换方法
+            tabChange(index){
+                // 开始切换
+                this.$router.push({
+                    path:'/member/sourceHome',
+                    query:{tabIndex:index},
+                })
+                // 在组件内切换
+                this.tabShowIndex = index
+            }
         },
         
     }
