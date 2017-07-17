b42316d68bf2d5c1af659947b964bc549f50b1f8
Geek.Yu <1098654043@qq.com>
2017-07-17 18:31:58 +0800

    代码更新

--- pro/src/components/webSidebar/index.vue
+++ pro/src/components/webSidebar/index.vue
@@ -3,19 +3,47 @@
 	    <div class="m-sidebar-wrap">
             <div class="m-expand-bg"></div><!-- 防抖动背景 -->
             <div class="m-sidebar-con">
-                
+                <ul class="u-side-lst f__clearfix">
+                    <li class="u-side-item u-member">
+                        <a href="javascript:;" class="u-lk">
+                            <div class="icon">
+                                <i class="iconfont icon-huiyuanzhongxin"></i>
+                            </div>
+                        </a>
+                    </li><!-- 会员中心 -->
+                    <li class="u-side-item u-collect">
+                        <a href="javascript:;" class="u-lk">
+                            <div class="icon">
+                                <i class="iconfont icon-shoucang11"></i>
+                            </div>
+                        </a>
+                    </li><!-- 收藏 -->
+                    <li class="u-side-item u-wallet">
+                        <a href="javascript:;" class="u-lk">
+                            <div class="icon">
+                                <i class="iconfont icon-qianbao8"></i>
+                            </div>
+                        </a>
+                    </li><!-- 钱包 -->
+                </ul>
             </div><!-- 真实的导航条内容 -->
             <div class="m-sidebar-other">
-                <div class="u-side-item u-ewm">
-                    <div class="icon">
-                        <i class="iconfont icon-erweima3"></i>
-                    </div>
-                </div><!-- 二维码 -->
-                <div class="u-side-item u-backTop">
-                    <div class="icon">
-                        <i class="iconfont icon-fanhuidingbu"></i>
-                    </div>
-                </div><!-- 返回顶部 -->
+                <ul class="u-side-lst f__clearfix">
+                    <li class="u-side-item u-ewm">
+                        <a href="javascript:;" class="u-lk">
+                            <div class="icon">
+                                <i class="iconfont icon-erweima3"></i>
+                            </div>
+                        </a>
+                    </li><!-- 二维码 -->
+                    <li class="u-side-item u-backTop">
+                        <a href="javascript:;" class="u-lk">
+                            <div class="icon">
+                                <i class="iconfont icon-fanhuidingbu"></i>
+                            </div>
+                        </a>
+                    </li><!-- 返回顶部 -->
+                </ul>
             </div><!-- 其他内容 -->
         </div>
 	</div>
