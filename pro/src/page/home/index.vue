9f9c18037a3f0637dae32b888b6d7e6ae302719c
Geek.Yu <1098654043@qq.com>
2017-07-22 11:38:30 +0800

    修改BUG

--- pro/src/page/home/index.vue
+++ pro/src/page/home/index.vue
@@ -195,7 +195,7 @@
                     pagination: '.swiper-pagination',  
                     slidesPerView: 'auto',  
                     centeredSlides: true,  
-                    autoplay : 228500,
+                    autoplay : 3500,
                     speed: 500,                         //速度
                     loop: true,                         //环路
                     paginationClickable: true,          //分页点击
